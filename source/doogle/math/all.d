ae2004959898fd3f7e1fe105e9ddc0dcad1632b5
rikki cattermole <pbdadmin@gmail.com>
2013-09-03 18:32:10 +1200

    Removing own math library. Using gl3n instead. (More complete and advance).

--- source/doogle/math/all.d
+++
@@ -1,7 +0,0 @@
-module doogle.math.all;
-public import doogle.math.vec2;
-public import doogle.math.vec3;
-public import doogle.math.vec4;
-public import doogle.math.mat2;
-public import doogle.math.mat3;
-public import doogle.math.mat4;
