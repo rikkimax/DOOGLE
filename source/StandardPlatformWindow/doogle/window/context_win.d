eb31e0837909cc9369fad5532db73ce3c6525ac6
rikki cattermole <pbdadmin@gmail.com>
2013-09-17 18:26:06 +1200

    Should be a little bit more safer now.

--- source/StandardPlatformWindow/doogle/window/context_win.d
+++ source/StandardPlatformWindow/doogle/window/context_win.d
@@ -12,18 +12,26 @@ version(Windows) {
 		this(ubyte color, ubyte depth, ubyte stencil, uint antialias, shared(platform.wgl.HDC) hdc) {
 			auto pixelAttribs =
 			[
-				platform.wglext.WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
-				platform.wglext.WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
-				platform.wglext.WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
-				platform.wglext.WGL_PIXEL_TYPE_ARB, platform.wglext.WGL_TYPE_RGBA_ARB,
-				platform.wglext.WGL_COLOR_BITS_ARB, color,
-				platform.wglext.WGL_DEPTH_BITS_ARB, depth,
-				platform.wglext.WGL_STENCIL_BITS_ARB, stencil,
-				platform.wglext.WGL_SAMPLE_BUFFERS_ARB, antialias > 1 ? GL_TRUE : GL_FALSE,
-				platform.wglext.WGL_SAMPLES_ARB, antialias > 1 ? antialias : 0,
-				0
+				platform.windows.PIXELFORMATDESCRIPTOR.sizeof,
+				1,
+				platform.wglext.WGL_DRAW_TO_WINDOW_ARB | platform.wglext.WGL_SUPPORT_OPENGL_ARB | platform.wglext.WGL_DOUBLE_BUFFER_ARB,
+				platform.wglext.WGL_TYPE_RGBA_ARB,
+				color,
+				0, 0, 0, 0, 0, 0,
+				0,
+				0,
+				0,
+				0, 0, 0, 0,
+				depth,
+				stencil,
+				antialias,
+				platform.windows.PFD_MAIN_PLANE,
+				0,
+				0, 0, 0
 			];
-			platform.windows.SetPixelFormat(cast(platform.windows.HDC)hdc, platform.windows.PIXELFORMATDESCRIPTOR.sizeof, cast(platform.windows.PIXELFORMATDESCRIPTOR*)pixelAttribs.ptr);
+
+			auto format = platform.windows.ChoosePixelFormat(cast(platform.windows.HDC)hdc, cast(platform.windows.PIXELFORMATDESCRIPTOR*)pixelAttribs.ptr);
+			platform.windows.SetPixelFormat(cast(platform.windows.HDC)hdc, format, cast(platform.windows.PIXELFORMATDESCRIPTOR*)pixelAttribs.ptr);
 			_context = cast(shared(platform.windows.HGLRC))platform.wgl.wglCreateContext(cast(platform.windows.HDC)hdc);
 			platform.wgl.wglMakeCurrent(cast(void*)hdc, cast(void*)_context);
 			_hdc = cast(shared(platform.windows.HDC))hdc;
