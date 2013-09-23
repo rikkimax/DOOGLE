6b5798f1006b2dae2a5cfa9ff5e27ebadfd22284
rikki cattermole <pbdadmin@gmail.com>
2013-09-24 02:24:20 +1200

    Moving compile time dynamic library linkage into dub's control.

--- source/doogle/platform.d
+++ source/doogle/platform.d
@@ -10,17 +10,10 @@ import derelict.freetype.ft;
 struct PlatformVersions {
 
 	version(Posix) {
-		pragma(lib, "dl");
-		pragma(lib, "X11");
-		pragma(lib, "Xrandr");
-		
 		public import glx = derelict.opengl3.glx;
 		public import glxext = derelict.opengl3.glxext;
 		public import core.sys.posix.sys.time;
 	} else version(Windows) {
-		pragma(lib, "gdi32");
-		pragma(lib, "user32");
-
 		public import wgl = derelict.opengl3.wgl;
 		public import wglext = derelict.opengl3.wglext;
 		public import windows = windows;
