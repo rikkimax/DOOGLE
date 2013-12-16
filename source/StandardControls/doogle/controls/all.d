c3e1e7ace5114239fb8aa5dbe72344cce743992e
rikki cattermole <pbdadmin@gmail.com>
2013-12-17 00:30:10 +1300

    Forgot that flow layout had ot be added to the standard controls package.

--- source/StandardControls/doogle/controls/all.d
+++ source/StandardControls/doogle/controls/all.d
@@ -1,5 +1,7 @@
 module doogle.controls.all;
 
+public import doogle.controls.layouts.flow;
+
 version(Windows) {
 	public import doogle.controls.opengl.defs.picture;
 	public import doogle.controls.opengl.defs.label;
